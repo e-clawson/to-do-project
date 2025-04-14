4e3038aba1c1ed448c656e2407c87fece6a5bb68
Elizabeth Clawson <emclawson1@gmail.com>
2025-04-14 17:26:31 +0530

    added test port to backend

--- to-do-backend/server.js
+++ to-do-backend/server.js
@@ -1,2 +1,19 @@
 //import express
 import express from "express";
+
+//creat our express application 
+const app = express()
+
+//specify a port
+const PORT = 8080
+
+//make any route to test 
+app.get('/test', (req, res) => {
+    res.json("hello")
+})
+
+//add the port 
+app.listen(PORT, () => {
+    console.log(`listening on port ${PORT}`)
+    //connectDB()
+})
