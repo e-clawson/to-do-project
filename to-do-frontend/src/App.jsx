879c189aa5e530bdcee221c53d6d53b1420a50d6
Elizabeth Clawson <emclawson1@gmail.com>
2025-04-15 15:11:24 +0530

    added text useEffect to App.jsx

--- to-do-frontend/src/App.jsx
+++ to-do-frontend/src/App.jsx
@@ -3,7 +3,19 @@ import { useEffect } from 'react'
 import './App.css'
 import Home from './Home'
 
+export const BASE_URL = import.meta.env.VITE_BASE_URL
+
 function App() {
+
+  useEffect(() => {
+    async function test() {
+      const response = await fetch(`${BASE_URL}/todos`)
+      const data = await response.json()
+      console.log(data)
+      setToDos(data)
+    }
+    test()
+  }, [])
 
   return (
     <>
