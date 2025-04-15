d8f5c1561faa87b0a3c7b20f74d4656d3e6727ed
Elizabeth Clawson <emclawson1@gmail.com>
2025-04-15 15:15:07 +0530

    added cors as middlewre in server.js

--- to-do-backend/server.js
+++ to-do-backend/server.js
@@ -4,6 +4,10 @@ import express from "express";
 import cors from 'cors';
 //allows us to make  our own env variables 
 
+//setup a cors middleware - can add some specific things into the () 
+//but for now we leave empty 
+app.use(cors())
+
 //creat our express application 
 const app = express()
 
