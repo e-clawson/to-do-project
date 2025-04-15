9f0b3439ce53f516b26ad7eb9964ed86793ae5e2
Elizabeth Clawson <emclawson1@gmail.com>
2025-04-15 14:45:47 +0530

    added todos to homepage

--- to-do-frontend/src/Home.jsx
+++ to-do-frontend/src/Home.jsx
@@ -2,11 +2,20 @@ import React from 'react'
 import Create from './Create'
 
 function Home() {
+    const [todos, setToDos] = useState([])
   return (
     <div>
         <h2>To-Do List: </h2>
         <h3>You Got This!</h3>
         <Create />
+        { 
+            todos.map(todo => 
+            (
+                <div>
+                    {todo}
+                </div>
+            ))
+        }
     </div>
   )
 }
