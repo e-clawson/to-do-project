e265b20265e6910a25076b660f96bd3e945c9a7f
Elizabeth Clawson <emclawson1@gmail.com>
2025-04-15 14:43:55 +0530

    imported create to homepage

--- to-do-frontend/src/Home.jsx
+++ to-do-frontend/src/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import Create from './Create'
 
 function Home() {
   return (
     <div>
         <h2>To-Do List: </h2>
         <h3>You Got This!</h3>
+        <Create />
     </div>
   )
 }
