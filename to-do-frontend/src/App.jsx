c31eff054f25771697a24a58dae7b62c9e8b8f3d
Elizabeth Clawson <emclawson1@gmail.com>
2025-04-15 15:30:01 +0530

    bug fix - updated the port to make sure frontend and backend are communicating

--- to-do-frontend/src/App.jsx
+++ to-do-frontend/src/App.jsx
@@ -3,13 +3,14 @@ import { useEffect } from 'react'
 import './App.css'
 import Home from './Home'
 
-export const BASE_URL = import.meta.env.VITE_BASE_URL
+export const BASE_URL = 'http://localhost:8080'
 
 function App() {
+  const [todos, setToDos] = useState([])
 
   useEffect(() => {
     async function test() {
-      const response = await fetch(`${BASE_URL}/todos`)
+      const response = await fetch(`${BASE_URL}/test`)
       const data = await response.json()
       console.log(data)
       setToDos(data)
