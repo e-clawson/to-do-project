4534cbb4c249f47bddce8b1332a5b724b556a6bc
Elizabeth Clawson <emclawson1@gmail.com>
2025-04-18 20:21:19 +0530

    updated App.jsx

--- to-do-frontend/src/App.jsx
+++ to-do-frontend/src/App.jsx
@@ -1,68 +1,62 @@
-import { useEffect, useState } from 'react'
-import CurrentUserContext from "./user-context.jsx"
+import { useState, useEffect } from 'react'
 import './App.css'
-import Login from "./components/login/Login.jsx"
+import {BrowserRouter, Routes, Route, useParams, Navigate} from "react-router-dom"
 
+import Header from './components/Header/Header'
+import Navbar from './components/Navbar/Navbar'
+import Exhibits from './components/Exhibits/Exhibits'
+import SignIn from './components/User/SignIn'
+import {auth} from "./config"
+import About from './components/Pages/About'
+import Events from './components/Pages/Events'
+import Home from './components/Pages/Home'
+import BridgeLifts from './components/Pages/BridgeLifts'
+import Rentals from './components/Pages/Rentals'
+import Contact from './components/Pages/Contact'
+import Footer from './components/Footer/Footer'
+import ExhibitPages from './components/Exhibits/ExhibitPages'
 
-export const BASE_URL = 'http://localhost:8080'
+export const BASE_URL = import.meta.env.VITE_BASE_URL || VITE_BASE_URL
 
 function App() {
-  const [currentUser, setCurrentUser] = useState(null) 
-  const [todos, setToDos] = useState([])
-   //better to be an empty array than null cuz you cant do map on a null
-   const [input, setInput] = useState('')
+  const [currentUser, setCurrentUser] = useState(null)
+  const [isAuthReady, setIsAuthReady] = useState(false)
+  const [exhibits, setExhibits] = useState([])
+  let {exhibitId} = useParams();
+
+  useEffect (() => {
+    let unsub = auth.onAuthStateChanged((user) => {
+      // console.log("authentication", user)
+      setCurrentUser(user)
+      setIsAuthReady(true)
+      unsub()
+    })
+  }, [])
+  // console.log("currentUser", currentUser)
 
   useEffect(() => {
     async function test() {
-      const response = await fetch(`${BASE_URL}/user/${currentUser.id}`)
+      const response = await fetch(`${BASE_URL}/exhibits`)
       const data = await response.json()
-      console.log(data)
-      setToDos(data)
+      setExhibits(data)
     }
     test()
   }, [])
-
-  function handleChange(e){ 
-    setInput(e.target.value)
-  }
-
-  async function handleSubmit(e){
-    //stop the page refresh 
-    e.preventDefault()
-    //formet our data - this should match the schema 
-    const todo = {
-      text: input,
-    }
-    //make the request  - fetch 
-    const response = await fetch(`${BASE_URL}/todos`, {
-      method: 'POST', 
-      body: JSON.stringify(todo),
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    })
-    const newToDo = await response.json()
-    setToDos([...todos, newToDo])
-    console.log(newToDo)
-    setInput("")
-  }
+  // console.log("initial load of exhibits", exhibits)
 
   return (
     <>
-    <CurrentUserContext.Provider value={{currentUser, setCurrentUser}}>
-    {/* <Header /> */}
-    <>
-    {currentUser !== null ? (
-      <>
-      {/* <TaskCounter todos={todos}/> */}
-      <TaskDisplay todos={todos} setTodos={setToDos}/>
-      <Signout />
-      </>
-    ) : <Login />}
-    </>
-    </CurrentUserContext.Provider>
+    <Navbar />
+    <Header />
+    <Routes>
+      <Route path="/" element={<Home  setCurrentUser={setCurrentUser} todos={todos}/>}/>
+      <Route path="/login" element={<SignIn setCurrentUser={setCurrentUser} />}/>
+      <Route path="/signup" element={<SignUp/>}/>
+      {/* <Route path="/exhibits" element={<Exhibits currentUser={currentUser} exhibits={exhibits} setExhibits={setExhibits}/>} />
+      <Route path="exhibits/:exhibitId" element={<ExhibitPages exhibitId={exhibitId} currentUser={currentUser} exhibits={exhibits} setExhibits={setExhibits}/>}/> */}
+    </Routes>
+    <Footer currentUser={currentUser} setCurrentUser={setCurrentUser}/>
     </>
   )
 }
-
 export default App
