0404eb7cfd0fb597d5e69a2bcb07fd6fa7455d20
Elizabeth Clawson <emclawson1@gmail.com>
2025-04-18 12:47:44 +0530

    added onChange to input field

--- to-do-frontend/src/components/Create.jsx
+++ to-do-frontend/src/components/Create.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Create() {
+  const [task, setTask] = useState()
+  const handleAdd = () => {
+
+  }
   return (
     <div>
-        <input type="text" name="" id=""/>
+        <input type="text" placeholder= "enter a task here" onChange={() => setTask(e.target.value)}/>
         <button type="button" onclick={handleAdd}>Add</button>
     </div>
   )
