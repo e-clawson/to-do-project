02bca4ab1b1c74d92c8e332f2e24652446fe5055
Elizabeth Clawson <emclawson1@gmail.com>
2025-04-19 18:34:11 +0530

    added is-auth route in auth routes

--- to-do-backend/routes/authRoutes.js
+++ to-do-backend/routes/authRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { signup, signin, signout, sendVerifyOtp, verifyEmail } from '../controllers/authController.js';
+import { signup, signin, signout, sendVerifyOtp, verifyEmail, isAthenticated } from '../controllers/authController.js';
 import userAuth from '../middleware/userauth.js';
 
 
@@ -11,5 +11,6 @@ authRouter.post('/signout', signout);
 
 authRouter.post('/send-verify-otp', userAuth, sendVerifyOtp); 
 authRouter.post('/verify-account', userAuth, verifyEmail); 
+authRouter.post('/is-auth', userAuth, isAthenticated);
 
 export default authRouter;
