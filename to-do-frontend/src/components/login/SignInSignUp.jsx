0badf601c418e4a003db287e3014416a374d5b6d
Elizabeth Clawson <emclawson1@gmail.com>
2025-04-19 13:42:06 +0530

    added ternary to the sign in and sign up buttons

--- to-do-frontend/src/components/login/SignInSignUp.jsx
+++ to-do-frontend/src/components/login/SignInSignUp.jsx
@@ -1,15 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './signInSignUp.css'
 
 import email_icon from '../../assets/email.png'
 import password_icon from '../../assets/password.png'
 
 const SignInSignUp = () => {
+    const [action, setAction] = useState("Sign Up")
   return (
     <>
     <div className='container'>
         <div className='header'>
-            <div className='text'>Sign Up</div>
+            <div className='text'>{action}</div>
             <div className ='underline'></div>
         </div>
         <div className='inputs'>
@@ -22,10 +23,10 @@ const SignInSignUp = () => {
                 <input type ='password' placeholder='Password'/>
             </div>
         </div>
-        <div className="forgotpassword">Forgot Password? <span>click here</span></div>
+        <div className="forgot-password">Forgot Password? <span>click here</span></div>
         <div className='submit-container'>
-            <div className='submit'>Sign Up</div>
-            <div className='submit'>Sign In</div>
+            <div className={action === "Sign In" ? "submit gray": "submit"}>Sign Up</div>
+            <div className={action === "Sign Up" ? "submit gray": "submit"}>Sign In</div>
         </div>
     </div>
     
