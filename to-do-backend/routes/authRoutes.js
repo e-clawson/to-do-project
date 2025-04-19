bfc038742f6594f5003387a3eaa30a84be62ee00
Elizabeth Clawson <emclawson1@gmail.com>
2025-04-19 18:29:21 +0530

    added routes for verify otp and account to authroutes

--- to-do-backend/routes/authRoutes.js
+++ to-do-backend/routes/authRoutes.js
@@ -1,10 +1,15 @@
 import express from 'express';
-import { signup, signin, signout } from '../controllers/authController.js';
+import { signup, signin, signout, sendVerifyOtp, verifyEmail } from '../controllers/authController.js';
+import userAuth from '../middleware/userauth.js';
+
 
 const authRouter = express.Router(); 
 
 authRouter.post('/signup', signup);
 authRouter.post('/signin', signin); 
 authRouter.post('/signout', signout); 
+
+authRouter.post('/send-verify-otp', userAuth, sendVerifyOtp); 
+authRouter.post('/verify-account', userAuth, verifyEmail); 
 
 export default authRouter;
