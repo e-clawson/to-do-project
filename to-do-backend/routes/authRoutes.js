7b440078a7fdacb0e3067e4511f60416e3abe604
Elizabeth Clawson <emclawson1@gmail.com>
2025-04-19 18:53:24 +0530

    added reset otp and reset password routes in authRoutes

--- to-do-backend/routes/authRoutes.js
+++ to-do-backend/routes/authRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { signup, signin, signout, sendVerifyOtp, verifyEmail, isAthenticated } from '../controllers/authController.js';
+import { signup, signin, signout, sendVerifyOtp, verifyEmail, isAthenticated, sendResetOtp, resetPassword } from '../controllers/authController.js';
 import userAuth from '../middleware/userauth.js';
 
 
@@ -12,5 +12,8 @@ authRouter.post('/signout', signout);
 authRouter.post('/send-verify-otp', userAuth, sendVerifyOtp); 
 authRouter.post('/verify-account', userAuth, verifyEmail); 
 authRouter.post('/is-auth', userAuth, isAthenticated);
+
+authRouter.post('/sent-reset-otp', sendResetOtp);
+authRouter.post('/reset-password', resetPassword);
 
 export default authRouter;
