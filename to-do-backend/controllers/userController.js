77f4101d505ebc956d0dac1c8b88d232de8bfd04
Elizabeth Clawson <emclawson1@gmail.com>
2025-04-19 18:59:02 +0530

    added user Controller and User Routes

---
+++ to-do-backend/controllers/userController.js
@@ -0,0 +1,26 @@
+import User from "../models/userModel.js";
+
+export const getUserData = async (req,res) => {
+    try {
+        const {userId} = req.body; 
+
+        const user = await User.findById(userId); 
+
+        if(!user){ 
+            return res.json({success: false, message: "User not found"})
+        }
+
+        res.json({
+            success: true, 
+            userData: { 
+                name: user.name, 
+                isAccountVerified: user.isAccountVerified,
+                
+            }
+        });
+
+    } catch (err) {
+        res.json({success: false, message: err.message})
+    }
+    
+}
