99d95fec349c2d73a701ebca1d142b5b4d7edc59
Elizabeth Clawson <emclawson1@gmail.com>
2025-04-19 12:51:41 +0530

    added signinsignup to app.jsx

--- to-do-frontend/src/App.jsx
+++ to-do-frontend/src/App.jsx
@@ -4,51 +4,19 @@ import {BrowserRouter, Routes, Route, useParams, Navigate} from "react-router-do
 
 // import Header from './components/Header/Header'
 // import Navbar from './components/Navbar/Navbar'
-import Login from './components/login/Login'
-import Home from './components/home/home'
+// import Login from './components/login/Login'
+// import Home from './components/home/Home'
 // import Footer from './components/Footer/Footer'
+import SignInSignUp from './components/login/SignInSignUp'
 
-export const BASE_URL = import.meta.env.VITE_BASE_URL || VITE_BASE_URL
+// export const BASE_URL = import.meta.env.VITE_BASE_URL || VITE_BASE_URL
 
 function App() {
-  const [currentUser, setCurrentUser] = useState(null)
-  const [todos, setToDos] = useState([])
-  let {toDoId} = useParams();
 
-  
-
-  // useEffect (() => {
-  //   let unsub = auth.onAuthStateChanged((user) => {
-  //     // console.log("authentication", user)
-  //     setCurrentUser(user)
-  //     setIsAuthReady(true)
-  //     unsub()
-  //   })
-  // }, [])
-  // console.log("currentUser", currentUser)
-
-  // useEffect(() => {
-  //   async function test() {
-  //     const response = await fetch(`${BASE_URL}/user/${userId}/todos`)
-  //     const data = await response.json()
-  //     setToDos(data)
-  //   }
-  //   test()
-  // }, [])
-  // console.log("initial load of todos", todos)
 
   return (
     <>
-    <Navbar />
-    <Header />
-    <Routes>
-      <Route path="/" element={<Home  setCurrentUser={setCurrentUser} todos={todos}/>}/>
-      <Route path="/login" element={<Login setCurrentUser={setCurrentUser} />}/>
-      <Route path="/signup" element={<SignUp/>}/>
-      {/* <Route path="/exhibits" element={<Exhibits currentUser={currentUser} exhibits={exhibits} setExhibits={setExhibits}/>} />
-      <Route path="exhibits/:exhibitId" element={<ExhibitPages exhibitId={exhibitId} currentUser={currentUser} exhibits={exhibits} setExhibits={setExhibits}/>}/> */}
-    </Routes>
-    <Footer currentUser={currentUser} setCurrentUser={setCurrentUser}/>
+      <SignInSignUp />
     </>
   )
 }
---
+++ to-do-frontend/src/components/login/SignInSignUp.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+
+const SignInSignUp = () => {
+  return (
+    <div>SignInSignUp</div>
+  )
+}
+
+export default SignInSignUp;
