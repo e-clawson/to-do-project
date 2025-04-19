45d27a2cb5d26acb6733d8441de2ce35362f2a52
Elizabeth Clawson <emclawson1@gmail.com>
2025-04-19 12:57:45 +0530

    added to the signInSignUp Form

--- to-do-frontend/src/components/login/SignInSignUp.jsx
+++ to-do-frontend/src/components/login/SignInSignUp.jsx
@@ -3,7 +3,22 @@ import React from 'react'
 
 const SignInSignUp = () => {
   return (
-    <div>SignInSignUp</div>
+    <div className='container'>
+        <div className='header'>
+            <div className='text'></div>
+            <div classname ='underline'></div>
+        </div>
+        <div className='inputs'>
+            <div className='input'>
+                <img src={email_icon} alt=""></img>
+                <input type ='email' />
+            </div>
+            <div className='input'>
+                <img src={password_icon} alt=""></img>
+                <input type ='password' />
+            </div>
+        </div>
+    </div>
   )
 }
 
