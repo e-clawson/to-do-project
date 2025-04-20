e9e622fffdd92032992e6094a09836fe395d6d81
Elizabeth Clawson <emclawson1@gmail.com>
2025-04-20 10:03:31 +0530

    added styling to header

--- to-do-frontend/src/components/header/Header.jsx
+++ to-do-frontend/src/components/header/Header.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import './header.css'
+import {assets} from '../../assets/assets'
 
 const Header = () => {
   return (
-    <h1>To-Do App</h1>
+    <div className='container'>
+        <h1>To-Do App</h1>
+        <img src={assets.check_list}></img>
+    </div>
+    
   )
 }
 
