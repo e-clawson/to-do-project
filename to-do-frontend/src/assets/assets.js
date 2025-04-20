a9d8312e40402e38446adc3669951e525a92270d
Elizabeth Clawson <emclawson1@gmail.com>
2025-04-20 09:58:29 +0530

    added checklist icon to assets

--- to-do-frontend/src/assets/assets.js
+++ to-do-frontend/src/assets/assets.js
@@ -2,11 +2,13 @@ import check_icon from './check.png'
 import email_icon from './email.png'
 import password_icon from './password.png'
 import person_icon from './person.png'
+import check_list from './check_list.png'
 
 
 export const assets = {
   check_icon, 
   email_icon,
   password_icon,
-  person_icon
+  person_icon,
+  check_list
 }
--- to-do-frontend/src/components/header/Header.jsx
+++ to-do-frontend/src/components/header/Header.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import './header.css'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <h1>To-Do App</h1>
   )
 }
 
