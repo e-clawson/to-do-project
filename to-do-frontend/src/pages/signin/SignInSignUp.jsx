de01253a352116ce0e87eed1e2e0cac6370151e9
Elizabeth Clawson <emclawson1@gmail.com>
2025-04-20 10:58:13 +0530

    adjusted styling

--- to-do-frontend/src/pages/signin/SignInSignUp.jsx
+++ to-do-frontend/src/pages/signin/SignInSignUp.jsx
@@ -3,7 +3,6 @@ import './signInSignUp.css'
 import {assets} from '../../assets/assets.js'
 
 const SignInSignUp = () => {
-    const [state, setState] = useState()
     const [action, setAction] = useState("Sign Up")
   return (
     <>
@@ -13,10 +12,14 @@ const SignInSignUp = () => {
             <div className ='underline'></div>
         </div>
         <div className='inputs'>
-        <div className='input'>
-                <img src={assets.person_icon} alt=""></img>
-                <input type ='text' placeholder='Full Name' required/>
-            </div>
+            {/* only ask for name when it is signup */}
+            {action === 'Sign Up' && (
+                  <div className='input'>
+                  <img src={assets.person_icon} alt=""></img>
+                  <input type ='text' placeholder='Full Name' required/>
+              </div>
+            )}
+      
             <div className='input'>
                 <img src={assets.email_icon} alt=""></img>
                 <input type ='email' placeholder='Email Address' required/>
@@ -26,9 +29,10 @@ const SignInSignUp = () => {
                 <input type ='password' placeholder='Password' required/>
             </div>
         </div>
-        {action === "Sign In" ? <div className="forgot-password">Forgot Password? <span>click here</span></div> : <div className='sign-in'><p>Already have an account?</p><span>Sign-In Here</span></div>}
+        {action === "Sign In" ? <div className="forgot-password">Forgot Password? <span>click here</span></div> : <div></div>}
+        {action === "Sign In" ? <div className="forgot-password">Don't have an account? <span onClick={() => {setAction("Sign Up")}}>Sign Up Here</span></div> : <div className='sign-in'><p>Already have an account?</p><span onClick={() => {setAction("Sign In")}}>Sign-In Here</span></div>}
         <div className='submit-container'>
-            <button className={action === "Sign In" ? "submit gray": "submit"} onClick={() => {setAction("Sign Up")}}>{state}</button>
+            <button className='submit' onClick={() => {setAction("Sign Up")}}>{action}</button>
             {/* <button className={action === "Sign Up" ? "submit gray": "submit"} onClick={() => {setAction("Sign In")}}>Sign In</button> */}
         </div>
     </div>
