3dde9da619fb25d05d0eed978ed997003eeb9b21
Elizabeth Clawson <emclawson1@gmail.com>
2025-04-21 14:20:31 +0530

    added variables from useContext

--- to-do-frontend/src/components/navbar/Navbar.jsx
+++ to-do-frontend/src/components/navbar/Navbar.jsx
@@ -1,8 +1,13 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import {assets} from '../../assets/assets'
 import './navbar.css'
+import { useNavigate } from 'react-router-dom';
+import { AppContent } from '../context/AppContext';
 
 const navbar = () => {
+  const navigate = useNavigate(); 
+  const {userData, BASE_URL, setUserData, setIsSignedIn} = useContext(AppContent)
+
   return (
     <div>
         <img src={assets.check_icon} alt="a green check icon in a circle" className='logo'></img>
