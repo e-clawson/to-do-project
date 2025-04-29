301644870a8bed5302c5aa74213460b759a2512e
Elizabeth Clawson <emclawson1@gmail.com>
2025-04-29 14:57:53 +0530

    updated nodemailer.js transporter with SMTP details

--- to-do-backend/config/nodemailer.js
+++ to-do-backend/config/nodemailer.js
@@ -1,14 +1,15 @@
 import nodemailer from 'nodemailer'
 
 const transporter = nodemailer.createTransport({
-    //need smtp details - we're gonna use brevo smtp?
-    host: 'host',
-    port: '', 
+    //need smtp details - we're gonna use brevo SMTP
+    //SMTP Server goes here 
+    host: 'smtp-relay.brevo.com',
+    //SMTP Port goes here: 
+    port: '587', 
     auth: { 
         user: process.env.SMTP_USER, 
-        password: process.env.SMTP_PASS, 
+        password: process.env.SMTP_PASSWORD, 
     }
-
 });
 
 export default transporter;
